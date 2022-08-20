41137b79cac3218849d5da9579a93eed774f6a6c
GiangNguyenn <giang.nguyen@skedulo.com>
2022-08-20 14:19:03 +0700

    Minor fix on displaying cart

--- src/main/java/Service/OrderService.java
+++ src/main/java/Service/OrderService.java
@@ -41,16 +41,16 @@ public class OrderService implements OrderInterface {
         }
         System.out.print("Please enter the index of the order: ");
         String targetOrderIndex = Utils.reader.readLine().trim();
-        if (!targetOrderIndex.matches("[0-9]+")){
+        if (!targetOrderIndex.matches("[0-9]+")) {
             System.out.println("Input Invalid!");
             viewCustomerOrder();
         }
-        if (Integer.parseInt(targetOrderIndex) > ordersOfCustomer.size()){
+        if (Integer.parseInt(targetOrderIndex) > ordersOfCustomer.size()) {
             System.out.println("\nOrder not found");
             System.out.println("Please enter again");
             viewCustomerOrder();
         }
-        Order order = ordersOfCustomer.get(Integer.parseInt(targetOrderIndex)-1);
+        Order order = ordersOfCustomer.get(Integer.parseInt(targetOrderIndex) - 1);
         System.out.println(order.toStringCustom());
     }
 
@@ -109,10 +109,10 @@ public class OrderService implements OrderInterface {
         PrintWriter out = new PrintWriter(BaseConstant.ORDER_DATA_PATH);
         if (!BaseHelper.isNullOrEmpty(lstOrder)) {
             for (Order order : lstOrder) {
-                out.write(order.getId()+","+order.getMemberID()+","+            //Stores userID for customer
-                        convertToString(order.getProductsID())+","+        //Stores ID for product
-                        convertToString(order.getCreated_at())+","+     //String of ISO dateformat
-                        order.getPaid()+","+ order.getTotalPrice()+"\n");
+                out.write(order.getId() + "," + order.getMemberID() + "," +            //Stores userID for customer
+                        convertToString(order.getProductsID()) + "," +        //Stores ID for product
+                        convertToString(order.getCreated_at()) + "," +     //String of ISO dateformat
+                        order.getPaid() + "," + order.getTotalPrice() + "\n");
             }
         }
         out.close();
@@ -136,7 +136,7 @@ public class OrderService implements OrderInterface {
         for (String product : productIDList) {
             resString.append(product).append("and");
         }
-        resString = new StringBuilder(resString.substring(0, resString.length()-3));        //Deleting the last "and"
+        resString = new StringBuilder(resString.substring(0, resString.length() - 3));        //Deleting the last "and"
         return resString.toString();
     }
 
@@ -202,23 +202,35 @@ public class OrderService implements OrderInterface {
         if (!BaseHelper.isNullOrEmpty(product)) {
             Utils.cart.add(productId);
             System.out.println("Product " + product.getProductName() + " added to cart!");
-            System.out.println(Utils.cart);
+            printCart();
         } else {
             System.out.println("Product Id not found! Please try again!");
 //            addProductToCart();
             // Deleting this because the user will be stuck in the loop if they don't know the ID
-
         }
+    }
+
+    private void printCart() {
+        for (String productId : Utils.cart) {
+            if (Utils.cart.size() > 0) {
+                System.out.println("Shopping cart: ");
+                System.out.println("-----------------------------------------");
+                System.out.println(BaseHelper.getProductByProductId(productId).getProductName());
+                System.out.println("-----------------------------------------");
+            } else {
+                System.out.println("Empty cart!");
+            }
+        }
+
     }
 
     public Double calculateTotalPrice() {
         List<Product> productObjectList = new ArrayList<>();
-        for (String ProductId : Utils.cart){
+        for (String ProductId : Utils.cart) {
             Product productObject = BaseHelper.getProductByProductId(ProductId);
             productObjectList.add(productObject);
         }
-        return productObjectList.stream().mapToDouble(Product::getPrice).sum()
-                *((Member) Utils.current_user).discountAmount();
+        return productObjectList.stream().mapToDouble(Product::getPrice).sum() * ((Member) Utils.current_user).discountAmount();
     }
 
 
@@ -226,9 +238,7 @@ public class OrderService implements OrderInterface {
         LocalDateTime now = LocalDateTime.now();
 
         if (!BaseHelper.isNullOrEmpty(Utils.cart)) {
-            Order newOrder = new Order(BaseHelper.generateIdForOrder(),
-                    Utils.current_user.getId(),
-                    new ArrayList<String>(Utils.cart),      //Mat em 1 buoi sang
+            Order newOrder = new Order(BaseHelper.generateIdForOrder(), Utils.current_user.getId(), new ArrayList<String>(Utils.cart),      //Mat em 1 buoi sang
                     now,                                    //When clearing Utils.cart, the products in this order are deleted too
                     false,                                  //Have to make separate object by copying the origin Utils.cart
                     this.calculateTotalPrice());
